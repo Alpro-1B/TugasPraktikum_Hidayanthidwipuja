6ba915772bca8492d0cc035cbbec626c15d94fb9
Hidayanthidwipuja <2310631170020@student.unsika.ac.id>
2023-10-14 22:35:06 +0700

    puja

---
+++ pertemuan-1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Nama : Hidayanthi dwi puja" << endl<<endl;
+    cout <<"NPM  : 2310631170020" <<endl<<endl;
+    cout <<"Kelas: B-Informatika" <<endl<<endl;
+    cout <<"Cita-cita : Guru" <<endl<<endl;
+    cout <<"Motivasi masuk Informatika : Melanjutkan jurusan" <<endl<<endl;
+    cout <<"--------------------------------------------------" <<endl<<endl;
+    return 0;
+}
